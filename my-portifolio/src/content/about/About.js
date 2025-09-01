f0aeb76317dc0499288df9ce2db7b4ca0c823dcd
Eric Araujo <eric.araujo@fitbank.com.br>
2025-09-01 15:43:10 -0300

    about att

--- my-portifolio/src/content/about/About.js
+++ my-portifolio/src/content/about/About.js
@@ -13,6 +13,8 @@ function About() {
                 <a href="https://www.linkedin.com/in/eric-albuquerque-b08087218/" target="_blank" rel="noreferrer"> 🟦 Linkedin</a>
               </div>
             <p>Olá! Meu nome é Eric. Sou um desenvolvedor full-stack e analista de qualidade Jr.</p>
+            <p>Atuo como Analista de QA e Dev, colaborando no desenvolvimento e na garantia de qualidade de sistemas bancários, incluindo Web, APIs e microsserviços. Utilizo tecnologias como C#, .Net, ASP Net, Javascript e SQL Server para desenvolver soluções robustas e realizar testes automatizados com PlayWright, garantindo a eficiência e a confiabilidade dos produtos.</p>
+            <p>Como estudante de Ciência da Computação na Universidade Federal do Ceará, busco alinhar minha formação acadêmica com meu trabalho, aprimorando habilidades em desenvolvimento de software e resolução de problemas. Sou motivado por desafios técnicos e pela oportunidade de contribuir em equipe para entregar sistemas de alta qualidade.</p>
           </div>
         </div>
         <div className="img-me">
